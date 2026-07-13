5999403c88d26bf1e1b3f6dd544af54062cac20e
nashparagas-eng <nash_paragas@dlsu.edu.ph>
2026-07-13 12:36:10 +0800

    made PokemonAbstract into an abstract class and added variable

--- src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/AbstractPokemon.java
+++ src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/AbstractPokemon.java
@@ -1,4 +1,13 @@
 package ph.edu.dlsu.lbycpob.pokemonwebapp.model;
 
-public class AbstractPokemon {
-}
+// Abstract Pokemon class
+public abstract class AbstractPokemon implements PokemonOperations {
+    protected int instanceId;
+    protected String name;
+    protected double weight;
+    protected double height;
+    protected double attack;
+    protected double defense;
+    protected double stamina;
+    protected String type;
+
