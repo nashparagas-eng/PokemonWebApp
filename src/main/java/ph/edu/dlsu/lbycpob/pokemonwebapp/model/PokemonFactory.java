abcd12168a7ab01fc13ec8bce0fc75aafcc77511
nashparagas-eng <nash_paragas@dlsu.edu.ph>
2026-07-13 12:33:46 +0800

    Added imports to PokemonFactory.java

--- src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/PokemonFactory.java
+++ src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/PokemonFactory.java
@@ -1,4 +1,14 @@
-package ph.edu.dlsu.lbycpob.pokemonwebapp.model;
+package ph.edu.dlsu.lbycpob.pokemonwebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ph.edu.dlsu.lbycpob.pokemonwebapp.model.Pokemon;
+import ph.edu.dlsu.lbycpob.pokemonwebapp.service.PokemonService;
+import java.util.List;
 
 public class PokemonFactory {
 }
