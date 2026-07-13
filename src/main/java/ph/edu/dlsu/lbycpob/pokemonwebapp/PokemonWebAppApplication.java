8032e299b170c20419174186dc3d49bbc96075c7
nashparagas-eng <nash_paragas@dlsu.edu.ph>
2026-07-13 12:23:55 +0800

    Initial commit

---
+++ src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/PokemonWebAppApplication.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.pokemonwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonWebAppApplication.class, args);
+    }
+
+}
