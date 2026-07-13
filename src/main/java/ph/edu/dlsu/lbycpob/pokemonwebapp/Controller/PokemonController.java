5a5667a22528d32bc2b6d0c34ce5d1c5add5a7d4
nashparagas-eng <nash_paragas@dlsu.edu.ph>
2026-07-13 12:42:58 +0800

    Moved PokemonController to Controller Package

--- src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/Controller/PokemonController.java
+++ src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/Controller/PokemonController.java
@@ -1,4 +1,22 @@
 package ph.edu.dlsu.lbycpob.pokemonwebapp.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ph.edu.dlsu.lbycpob.pokemonwebapp.model.Pokemon;
+import ph.edu.dlsu.lbycpob.pokemonwebapp.service.PokemonService;
+
+import java.util.List;
+
 public class PokemonController {
-}
+    @Controller
+    public static class PokemonController {
+
+        private final PokemonService pokemonService;
+
+        public PokemonController(PokemonService pokemonService) {
+            this.pokemonService = pokemonService;
+        }
