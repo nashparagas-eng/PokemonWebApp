8bc2d084893f6e7676c80826ff3886b0ba99a7c2
nashparagas-eng <nash_paragas@dlsu.edu.ph>
2026-07-13 12:45:16 +0800

    Added imports to PokemonService

--- src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/service/PokemonService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/service/PokemonService.java
@@ -1,4 +1,13 @@
 package ph.edu.dlsu.lbycpob.pokemonwebapp.service;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+import ph.edu.dlsu.lbycpob.pokemonwebapp.model.Pokemon;
+import ph.edu.dlsu.lbycpob.pokemonwebapp.model.PokemonFactory;
+import ph.edu.dlsu.lbycpob.pokemonwebapp.utils.CsvLoader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class PokemonService {
 }
