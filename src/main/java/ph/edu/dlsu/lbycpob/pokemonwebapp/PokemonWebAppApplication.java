b3159553c2093e054dd82dcea5f902488ba21722
nashparagas-eng <nash_paragas@dlsu.edu.ph>
2026-07-13 12:32:50 +0800

    Cleaned SpringBoot

--- src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/PokemonWebAppApplication.java
+++ src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/PokemonWebAppApplication.java
@@ -1,13 +1,10 @@
 package ph.edu.dlsu.lbycpob.pokemonwebapp;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PokemonWebAppApplication {
 
+public class PokemonWebAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(PokemonWebAppApplication.class, args);
-    }
-
-}
+    }}
