88e6e90bc2972fc12c7bb43525f795b56049f184
nashparagas-eng <nash_paragas@dlsu.edu.ph>
2026-07-13 13:13:42 +0800

    Added pokemon to enxtend to ebstractPokemon

--- src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/Pokemon.java
+++ src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/Pokemon.java
@@ -1,4 +1,19 @@
 package ph.edu.dlsu.lbycpob.pokemonwebapp.model;
 
-public class Pokemon {
+// Concrete Pokemon implementation
+public class Pokemon extends AbstractPokemon {
+
+    public Pokemon(int instanceId, String name, double weight, double height, double attack, double defense, double stamina, String type) {
+        super(instanceId, name, weight, height, attack, defense, stamina, type);
+    }
+
+    @Override
+    public String getSpecialAbility() {
+        return "";
+    }
+
+    @Override
+    public String getTypeBackground() {
+        return "";
+    }
 }
