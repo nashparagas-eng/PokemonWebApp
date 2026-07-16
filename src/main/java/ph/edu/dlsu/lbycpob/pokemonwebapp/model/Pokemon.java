88e496265c34f45b039eb11bca3720ddd4725293
Tin <francis_martin_ang@dlsu.edu.ph>
2026-07-16 17:24:29 +0800

    Added the isDualType boolean

--- src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/Pokemon.java
+++ src/main/java/ph/edu/dlsu/lbycpob/pokemonwebapp/model/Pokemon.java
@@ -52,6 +52,10 @@ public class Pokemon extends AbstractPokemon {
         }
         return getTypeBackground(types[1].toUpperCase());
     }
+    public boolean isDualType() {
+        return type.contains("-");
+    }
+
 
 
 }
